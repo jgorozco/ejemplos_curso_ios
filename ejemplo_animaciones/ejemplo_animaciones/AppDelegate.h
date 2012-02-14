a66c653d82b69dc9c3683b95731ca61c3596dcc9
jose Garcia Orozco <josegoupm@gmail.com>
2012-02-14 09:45:03 +0100

    first commit

---
+++ ejemplo_animaciones/ejemplo_animaciones/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ejemplo_animaciones
+//
+//  Created by jose garcia on 14/02/12.
+//  Copyright (c) 2012 freelance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ejemplo_animaciones/ejemplo_animaciones/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ejemplo_animaciones
+//
+//  Created by jose garcia on 14/02/12.
+//  Copyright (c) 2012 freelance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *ball1;
+@property (weak, nonatomic) IBOutlet UIButton *ball2;
+@property (weak, nonatomic) IBOutlet UIButton *ball3;
+
+@end
