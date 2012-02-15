1e4c96f2c1d176c7a93efc327daa876e4375b505
jose Garcia Orozco <josegoupm@gmail.com>
2012-02-15 20:33:17 +0100

    animaciones y soniod

--- ejemplo_animaciones/ejemplo_animaciones/ViewController.h
+++ ejemplo_animaciones/ejemplo_animaciones/ViewController.h
@@ -9,8 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    BOOL continuarAnimacion;
+
+
+}
 @property (weak, nonatomic) IBOutlet UIButton *ball1;
 @property (weak, nonatomic) IBOutlet UIButton *ball2;
 @property (weak, nonatomic) IBOutlet UIButton *ball3;
 
+- (IBAction)touchBall1:(id)sender;
+
+- (IBAction)touchBall2:(id)sender;
+- (IBAction)touchBall3:(id)sender;
+-(void)animarFase1Bola;
+-(void)animarFase2Bola;
+-(void)ejecutarSonido;
 @end
--- ejemplo_animaciones/ejemplo_animaciones/ViewController.m
+++ ejemplo_animaciones/ejemplo_animaciones/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
+#define DegreesToRadians(X) (X*M_PI/180.0) 
 @implementation ViewController
 @synthesize ball1;
 @synthesize ball2;
@@ -28,12 +29,119 @@
 }
 
 - (IBAction)touchBall1:(id)sender {
+    [UIView animateWithDuration:1.7 
+                          delay:1.0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+         
+         ball1.center=CGPointMake(ball1.center.x+80, ball1.center.y+80);
+     }      
+    completion:^(BOOL finished) 
+    {
+     ball1.center=CGPointMake(50,50);
+    
+    }
+     
+     ];
+    
+
 }
 
 
 - (IBAction)touchBall2:(id)sender {
+   continuarAnimacion=NO;
+    [self ejecutarSonido];
+    [UIView animateWithDuration:1.7 
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+         
+         CGAffineTransform swingTransform = CGAffineTransformIdentity;
+         swingTransform = CGAffineTransformRotate(swingTransform, DegreesToRadians(90));
+         ball2.transform = swingTransform;   
+         ball2.center=CGPointMake(ball2.center.x-80, ball2.center.y-80);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         ball2.center=CGPointMake(150,150);
+         CGAffineTransform swingTransform = CGAffineTransformIdentity;
+          ball2.transform = swingTransform;   
+     }
+     
+     ];
+
+    
 }
+
+
+-(void)ejecutarSonido
+{
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                         pathForResource:@"click"
+                                         ofType:@"mp4"]];
+    
+    NSError *error;
+    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc]
+                                  initWithContentsOfURL:url
+                                  error:&error];
+    [audioPlayer prepareToPlay];
+    [audioPlayer play];
+    
+
+}
+
+-(void)animarFase1Bola
+{
+
+//    NSLog(@"el valor de numeroVeces es:%d",numeroVeces);
+
+    if (continuarAnimacion){
+        [UIView animateWithDuration:1.7 
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+         {
+         
+         ball3.center=CGPointMake(ball3.center.x+80, ball3.center.y+80);
+         }      
+                     completion:^(BOOL finished) 
+         {
+         [self animarFase2Bola];
+         
+        }
+     
+         ];
+    }
+
+}
+
+
+-(void)animarFase2Bola
+{
+
+    [UIView animateWithDuration:1.7 
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+         
+         ball3.center=CGPointMake(ball3.center.x-80, ball3.center.y-80);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         [self animarFase1Bola];
+         
+     }
+     
+     ];
+
+}
+
 - (IBAction)touchBall3:(id)sender {
+    continuarAnimacion=YES;
+    [self animarFase1Bola];
 }
 
 - (void)viewDidUnload
