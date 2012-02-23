0206c3c8627c03b4e68108461c04d93cf1016763
jose Garcia Orozco <josegoupm@gmail.com>
2012-02-23 20:52:36 +0100

    clase 3. animaciones, sonido nuevo de pajarito y drag and drop

--- ejemplo_animaciones/ejemplo_animaciones/ViewController.h
+++ ejemplo_animaciones/ejemplo_animaciones/ViewController.h
@@ -7,22 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController : UIViewController
 {
     BOOL continuarAnimacion;
-
+    
+    NSTimer *timer;//timer, temporizador.
+    CGRect projectileFrame;
+    CGPoint actualPointBall2;// punto actual donde hemos tocado.
+    AVAudioPlayer *audioPlayer;//Declarar una variable
 
 }
 @property (weak, nonatomic) IBOutlet UIButton *ball1;
 @property (weak, nonatomic) IBOutlet UIButton *ball2;
 @property (weak, nonatomic) IBOutlet UIButton *ball3;
-
+@property (weak, nonatomic) IBOutlet UIImageView *ball_image;
 - (IBAction)touchBall1:(id)sender;
 
 - (IBAction)touchBall2:(id)sender;
 - (IBAction)touchBall3:(id)sender;
+//evento cuando tocas
+-(IBAction)beginTouchBall2:(id) sender withEvent:(UIEvent *) event;
+//elevento cuando arrastras
+-(IBAction)moveBall2:(id) sender withEvent:(UIEvent *) event;
+//evento cuando termina
+-(IBAction)stopMoveBall2:(id) sender withEvent:(UIEvent *) event;
+
+
 -(void)animarFase1Bola;
 -(void)animarFase2Bola;
 -(void)ejecutarSonido;
+-(void)ponerEncimaBoton;
+-(void)empezarAnimacionRepetitiva;
 @end
--- ejemplo_animaciones/ejemplo_animaciones/ViewController.m
+++ ejemplo_animaciones/ejemplo_animaciones/ViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "ViewController.h"
-#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
 #define DegreesToRadians(X) (X*M_PI/180.0) 
 @implementation ViewController
 @synthesize ball1;
 @synthesize ball2;
 @synthesize ball3;
-
+@synthesize ball_image;
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -25,51 +26,127 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self empezarAnimacionRepetitiva];
+//    [NSThread detachNewThreadSelector:<#(SEL)#> toTarget:<#(id)#> withObject:<#(id)#>]
+    timer=[NSTimer scheduledTimerWithTimeInterval:0.05 //temporizador. 
+                            target:self//quien va a recibir evento.
+                          selector:@selector(ponerEncimaBoton) //funcion que queremos que se ejecute cada intervalo de tiempo
+                          userInfo:nil repeats:YES];
+    /*Es similar a poner [self ponerEncimaBoton] pero ejecutandose de manera secuencial, o en un hilo a parte...*/
+}
+-(void)ponerEncimaBoton
+{
+    projectileFrame = [[ball_image.layer presentationLayer] frame];
+    //cogemos el frame de la bola en movimiento
+ //   NSLog(@"Pongo encima en:x:%f  y:%f",projectileFrame.origin.x,projectileFrame.origin.y);
+
+    [ball1 setFrame:projectileFrame];
+    //ponemos el frame
+
+}
+
+
+-(void)empezarAnimacionRepetitiva
+{
+ball_image.center=CGPointMake(-ball_image.bounds.size.width, 53);
+    
+     [UIView animateWithDuration:4.0
+                          delay:0
+                        options:( UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionAutoreverse)
+                     animations:^
+     {      
+         ball_image.center=CGPointMake(320+ball_image.bounds.size.width, 80);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         if (finished){
+         [self empezarAnimacionRepetitiva];
+         }
+     }
+     
+     ];
+   
+
 }
 
 - (IBAction)touchBall1:(id)sender {
-    continuarAnimacion=NO;
-    [UIView animateWithDuration:0.5
-                          delay:0
-                       
-                        options:UIViewAnimationOptionAutoreverse
-                        //options:UIViewAnimationOptionCurveEaseInOut
-                     animations:^
-     {
-         
-         ball1.center=CGPointMake(ball1.center.x+200, ball1.center.y+300);
-     }      
-    completion:^(BOOL finished) 
-    {
-     ball1.center=CGPointMake(130,130);
+    NSLog(@"touchBall1");
+    //elimina las animaciones de este elemento
+    [ball_image.layer removeAllAnimations];
+    //para la ejecucion del timer.
+    [timer invalidate];
     
-    }
+    [ball_image setFrame:projectileFrame];
+    [UIView animateWithDuration:3.0
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^
+     {      
+         
+         [ball_image setHighlighted:YES];
+         ball_image.frame=CGRectMake(projectileFrame.origin.x, 
+                                     projectileFrame.origin.y, 0.0, 0.0);
+     }      
+                     completion:^(BOOL finished) 
+     {
+     }
      
      ];
-    
 
 }
 
+-(IBAction)beginTouchBall2:(id) sender withEvent:(UIEvent *) event
+{
+    actualPointBall2=[[[event allTouches] anyObject] locationInView:[self view]];
+}
+
+-(IBAction)moveBall2:(id) sender withEvent:(UIEvent *) event
+{
+    //cogemos otro punto
+    CGPoint punto=[[[event allTouches] anyObject] locationInView:[self view]];
+    //creamos un punto, que es la diferencia en x e y al punto inicial
+    CGPoint diference=CGPointMake(actualPointBall2.x-punto.x,
+                                  actualPointBall2.y-punto.y);
+    NSLog(@"movemos hacia:x%f  y:%f",diference.x,diference.y);
+   actualPointBall2=punto;//actualizamos punto actual, que se ha movido
+/*   \---comentando esto
+ Si no voy actualizando el punto incial, las diferencias enpre el punto inicial y el final son cada vez más grandes.
+ */    
+    ball2.center=CGPointMake(ball2.center.x-diference.x,
+                             ball2.center.y-diference.y)  ;
+}
+-(IBAction)stopMoveBall2:(id) sender withEvent:(UIEvent *) event
+{
+    NSLog(@"Soltamos en :x:%f  y:%f",ball2.center.x,ball2.center.y);
+    [self ejecutarSonido];
+}
+
+/*
+ 
+ 
+ 
+ 
+ 
+ */
 
 - (IBAction)touchBall2:(id)sender {
    continuarAnimacion=NO;
-    [self ejecutarSonido];
-    [UIView animateWithDuration:3 
-                          delay:0.5
-                        options:UIViewAnimationOptionAutoreverse
-                        //options:UIViewAnimationOptionCurveEaseInOut
+//    [self ejecutarSonido];
+    [UIView animateWithDuration:1.7 
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseInOut
                      animations:^
      {
          
          CGAffineTransform swingTransform = CGAffineTransformIdentity;
-         swingTransform = CGAffineTransformRotate(swingTransform, DegreesToRadians(40));
+         swingTransform = CGAffineTransformRotate(swingTransform, DegreesToRadians(90));
          ball2.transform = swingTransform;   
          ball2.center=CGPointMake(ball2.center.x-80, ball2.center.y-80);
      }      
                      completion:^(BOOL finished) 
      {
-         ball2.center=CGPointMake(200,200);
+         ball2.center=CGPointMake(150,150);
          CGAffineTransform swingTransform = CGAffineTransformIdentity;
           ball2.transform = swingTransform;   
      }
@@ -82,16 +159,14 @@
 
 -(void)ejecutarSonido
 {
-    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]
-                                         pathForResource:@"click"
-                                         ofType:@"mp4"]];
-    
-    NSError *error;
-    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc]
-                                  initWithContentsOfURL:url
-                                  error:&error];
+    NSURL *url=[[NSURL alloc]initFileURLWithPath:[[NSBundle mainBundle] 
+                                                 pathForResource:@"bird" ofType:@"mp3"] ];
+    audioPlayer=[[AVAudioPlayer alloc]initWithContentsOfURL:url error:nil];    
+//audioPlayer.volume=0.6;
     [audioPlayer prepareToPlay];
     [audioPlayer play];
+
+   
     
 
 }
